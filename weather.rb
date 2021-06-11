6eab5c684cef4323d7c1b19f58d69d7b149ae17c
Adrian <aguerra_57@yahoo.com>
2021-06-10 19:56:43 -0500

    hide API key

--- weather.rb
+++ weather.rb
@@ -1,10 +1,15 @@
 require 'http'
 
-response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=chicago&units=imperial&appid=")
+response = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=#{ENV['OPEN_WEATHER_API_KEY']}&units=imperial")
 
 data = response.parse(:json)
 
 p data 
 
 city_name = data['name']
- p city_name
+
+p city_name
+
+temperature = data['main']['temp']
+
+p temperature
